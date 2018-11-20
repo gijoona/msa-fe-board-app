b51936406051bfe989769118291f365ee5950d3f
In joon. Gong <ijgong@nglp.kr>
2018-11-20 15:16:11 +0900

    issue-list component 처리

--- src/components/issue/IssueList.vue
+++ src/components/issue/IssueList.vue
@@ -5,18 +5,30 @@
         <b-btn size="sm">New</b-btn>
       </router-link>
     </div>
-    <b-list-group>
-      <b-list-group-item v-for="issue in issues" :key="issue._id" @click="onEdit(issue)" button>
-        <div class="d-flex w-100 justify-content-between">
-          <h5 class="mb-1">{{ issue.title }}</h5>
-          <small>{{ issue.inputDt }}</small>
-        </div>
-      </b-list-group-item>
-    </b-list-group>
+    <item-list :list="issues" @edit="onEdit"></item-list>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+
+Vue.component('item-list', {
+  props: ['list'],
+  template: `    <b-list-group>
+        <b-list-group-item v-for="item in list" :key="item._id" @click="selectedItem(item)" button>
+          <div class="d-flex w-100 justify-content-between">
+            <h5 class="mb-1">{{ item.title }}</h5>
+            <small>{{ item.inputDt }}</small>
+          </div>
+        </b-list-group-item>
+      </b-list-group>`,
+  methods: {
+    selectedItem: function (item) {
+      this.$emit('edit', item)
+    }
+  }
+})
+
 export default {
   name: 'IssueList',
   data: function () {
