89f9c56813f28d4a3f279097c23d2e8cc528bca2
In joon. Gong <ijgong@nglp.kr>
2018-11-22 08:40:34 +0900

    로컬테스트 시 8001 포트의 gate를 바라보도록 proxytable 변경 화면상단에 nav-bar 추가

--- src/components/issue/IssueEdit.vue
+++ src/components/issue/IssueEdit.vue
@@ -1,10 +1,29 @@
 <template>
   <div>
-    <div>
-      <router-link :to="{ name: 'IssueList', params: {} }">
-        <b-btn size="sm">List</b-btn>
-      </router-link>
-    </div>
+    <b-navbar toggleable="md" type="dark" variant="info" id="navigation_bar">
+
+      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+      <b-navbar-brand href="/main">A Quester</b-navbar-brand>
+
+      <b-collapse is-nav id="nav_collapse">
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+
+          <b-nav-form>
+            <b-button-group class="mr-sm-2">
+              <router-link :to="{ name: 'IssueList', params: {} }">
+                <b-btn size="sm">List</b-btn>
+              </router-link>
+            </b-button-group>
+            <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+        </b-navbar-nav>
+
+      </b-collapse>
+    </b-navbar>
     <b-form @submit="onSubmit">
       <b-form-group id="titleGroup" horizontal
                     label="Title"
