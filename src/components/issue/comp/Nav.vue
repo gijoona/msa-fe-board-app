b6c330f44dd2ac0a6f299becc840ec505f5eb62c
In joon. Gong <ijgong@nglp.kr>
2018-11-23 15:59:35 +0900

    nav에 fixed=top설정 추가함에 따른 margin 추가

--- src/components/issue/comp/Nav.vue
+++ src/components/issue/comp/Nav.vue
@@ -1,18 +1,18 @@
 <template>
-  <b-navbar toggleable="md" type="dark" variant="info" id="navigation_bar">
+  <b-navbar toggleable="md" type="dark" variant="info" id="navigation_bar" fixed="top">
 
     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-    <b-navbar-brand href="/main">A Quester</b-navbar-brand>
+    <b-navbar-brand href="/main">WorkSpace</b-navbar-brand>
 
     <b-collapse is-nav id="nav_collapse">
 
       <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
 
-        <b-nav-item :to="{ name: 'IssueEdit', params: {} }">새글</b-nav-item>
+        <!-- <b-nav-item :to="{ name: 'IssueEdit', params: {} }">새글</b-nav-item>
         <b-nav-item :to="{ name: 'IssueList', params: {} }">목록</b-nav-item>
-        <b-nav-item :to="{ name: 'IssueEdit', params: {id: this.$route.params.id} }" v-show="isView">수정</b-nav-item>
+        <b-nav-item :to="{ name: 'IssueEdit', params: {id: this.$route.params.id} }" v-show="isView">수정</b-nav-item> -->
 
         <b-nav-form @submit="onSubmit" class="ml-3">
           <b-input-group>
@@ -40,12 +40,6 @@ export default {
     onSubmit: function (evt) {
       evt.preventDefault()
       this.$emit('search', this.searchTxt)
-    }
-  },
-  computed: {
-    isView: function () {
-      // View 화면일 경우에만 수정버튼 노출
-      return this.$route.name === 'IssueView'
     }
   }
 }
