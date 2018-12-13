f3a61e9869d6beb22cdd8f4ecd9c44fd6debd8f9
In joon. Gong <ijgong@nglp.kr>
2018-12-13 13:32:26 +0900

    font-awesome 추가

--- src/main.js
+++ src/main.js
@@ -6,6 +6,14 @@ import App from './App'
 import router from './router'
 import axios from 'axios'
 
+// fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(fas)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 
