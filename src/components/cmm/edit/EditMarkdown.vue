b97448405264b695307d868279bf0edaed6c144d
In joon. Gong <ijgong@nglp.kr>
2018-12-21 11:28:18 +0900

    markdown codeblock을 표시하기위한 CSS 추가 textarea를 p태그로 변경 ( contenteditable 속성사용 )

--- src/components/cmm/edit/EditMarkdown.vue
+++ src/components/cmm/edit/EditMarkdown.vue
@@ -2,7 +2,7 @@
   <b-form-group :id="currId+'Group'" horizontal :label="label" :label-for="'tar'+currId">
     <b-row>
       <b-col :cols="editCols">
-        <b-form-textarea :id="'tar'+currId" v-model="content" rows="5" max-rows="10" @input="onInput"></b-form-textarea>
+        <p :id="'tar'+currId" class="editor" @input="onInput" v-text="content" contenteditable></p>
         <b-btn class="preview-btn px-2 py-0" variant="link" @click="showCollapse = !showCollapse"
           :aria-controls="'collapse'+currId"
           :aria-expanded="showCollapse ? 'true' : 'false'">
@@ -11,7 +11,7 @@
       </b-col>
       <b-col :cols="preCols">
         <div :style="{'display' : showCollapse ? 'inline' : 'none'}">
-          <vue-markdown class="preview" :show="showCollapse" :source="content" :html="mdOpts.html"></vue-markdown>
+          <vue-markdown class="preview" :show="showCollapse" :source="markdownText" :html="mdOpts.html"></vue-markdown>
         </div>
       </b-col>
     </b-row>
@@ -26,6 +26,7 @@ export default {
   data: function () {
     return {
       content: '',
+      markdownText: '',
       mdOpts: {
         show: true,
         html: false,
@@ -38,17 +39,13 @@ export default {
       showCollapse: false
     }
   },
-  watch: {
-    data: function () {
-      this.content = this.data
-    }
-  },
   methods: {
     allRight: function (htmlStr) {
       console.log('markdown is parsed !')
     },
-    onInput: function () {
-      this.$emit('update:data', this.content)
+    onInput: function (event) {
+      this.markdownText = event.target.innerText
+      this.$emit('update:data', event.target.innerText)
     }
   },
   computed: {
@@ -61,10 +58,23 @@ export default {
   },
   components: {
     VueMarkdown
+  },
+  mounted: function () {
+    this.content = this.data
   }
 }
 </script>
 <style>
+.editor {
+  padding: 2px 10px;
+  overflow: auto;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  text-align: left;
+  height: 100%;
+  max-height: 254px;
+}
 .preview {
   padding: 2px 10px;
   overflow: auto;
@@ -72,7 +82,7 @@ export default {
   border: 1px solid #ccc;
   border-radius: 4px;
   text-align: left;
-  height: 134px;
+  min-height: 134px;
   max-height: 254px;
 }
 .full-height {
@@ -81,9 +91,37 @@ export default {
 .preview-btn {
   position: relative;
   float: right;
-  top: -40px;
-  right: 10px;
+  top: -55px;
+  right: 5px;
   z-index: 10;
   opacity: 0.5;
+}
+code {
+    padding: 2px 4px;
+    font-size: 90%;
+    color: #c7254e;
+    background-color: #f9f2f4;
+    border-radius: 4px;
+}
+pre {
+    display: block;
+    padding: 9.5px;
+    margin: 0 0 10px;
+    font-size: 13px;
+    line-height: 1.42857143;
+    color: #333;
+    word-break: break-all;
+    word-wrap: break-word;
+    background-color: #f5f5f5;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+}
+pre code {
+    padding: 0;
+    font-size: inherit;
+    color: inherit;
+    white-space: pre-wrap;
+    background-color: transparent;
+    border-radius: 0;
 }
 </style>
